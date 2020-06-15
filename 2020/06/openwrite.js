3a310e0833cab615cade47ef720bdcee09afa83c
poetries <jingguanliuye@gmail.com>
2020-06-15 22:23:58 +0800

    update

---
+++ 2020/06/openwrite.js
@@ -0,0 +1,8 @@
+const btw = new BTWPlugin();
+btw.init({
+    id: 'container',
+    blogId: '22699-1592137983091-414',
+    name: '前段进阶之旅',
+    qrcode: 'https://blog.poetries.top/img-repo/2020/06/qrcode.jpg',
+    keyword: '验证码',
+});
